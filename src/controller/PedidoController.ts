4544217b921e1b0b790c3143bba207c32e5ff0c4
FernandoSchuab <fschuab@gmail.com>
2020-07-21 21:21:04 -0300

    ver

--- src/controller/PedidoController.ts
+++ src/controller/PedidoController.ts
@@ -49,14 +49,14 @@ export class PedidoController extends BaseController<pedidos> {
         let _request = <pedidos>request.body;
         _request.descontoAplicado = 0; //por enquanto não implementamos cupons de desconto
     
-        // super.isRequired(_request.codCadastro, 'Preciso saber quem é você. Faça login');
-        // super.isRequired(_request.formaPagamento, 'Informe a forma pagamento');
-        // super.isRequired(_request.precoTotalPedido, 'Erro com preço total');
-        // if(_request.precoTotalPedido <= 0)
-        //     return {status: 404, message: 'Pedido Inválido'};
+        super.isRequired(_request.codCadastro, 'Preciso saber quem é você. Faça login');
+        super.isRequired(_request.formaPagamento, 'Informe a forma pagamento');
+        super.isRequired(_request.precoTotalPedido, 'Erro com preço total');
+        if(_request.precoTotalPedido <= 0)
+            return {status: 404, message: 'Pedido Inválido'};
 
-        // if (!_request.uid)
-        //   _request.statusPedido = 1;
+        if (!_request.uid)
+          _request.statusPedido = 1;
         return super.save(_request, request);
       }
     
