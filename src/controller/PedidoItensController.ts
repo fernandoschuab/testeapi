38e09b429e5f794ade4325bd77d043b81be422df
FernandoSchuab <fschuab@gmail.com>
2020-07-23 16:02:26 -0300

    ver

--- src/controller/PedidoItensController.ts
+++ src/controller/PedidoItensController.ts
@@ -29,10 +29,10 @@ export class PedidoItensController extends BaseController<itenspedidos> {
         _itenspedidos.codPedido = codPedido;
         console.log('passei até aqui')
         //_itenspedidos.uid = request.uid;
-        super.isRequired(_itenspedidos.codPedido, request.body);
-        super.isRequired(_itenspedidos.codItem, 'Informe o item');
-        super.isRequired(_itenspedidos.quantidadeProduto, 'Informe a quantidade do item');
-        super.isRequired(_itenspedidos.precoTotalItem, 'Erro preço produto');
+        // super.isRequired(_itenspedidos.codPedido, request.body);
+        // super.isRequired(_itenspedidos.codItem, 'Informe o item');
+        // super.isRequired(_itenspedidos.quantidadeProduto, 'Informe a quantidade do item');
+        // super.isRequired(_itenspedidos.precoTotalItem, 'Erro preço produto');
         return (super.save(_itenspedidos, request));
       }
     
